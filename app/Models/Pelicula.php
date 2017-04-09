be6b85c95a177a4ae80c5fdd7bbfcc21ff4355d5
iVan Vitali <webcoder.ivan@gmail.com>
2017-04-09 17:01:32 -0300

    dev#4 Se creo el modelo peliculas dentro de la carpeta app/Models

---
+++ app/Models/Pelicula.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Pelicula extends Model
+{
+
+    use SoftDeletes;
+    /**
+     * The database column used for Softdeleting.
+     *
+     * @var string
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'peliculas';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'titulo', 'descripcion',
+    ];
+
+    /**
+     * Set FirstName attribute
+     *
+     * @param string
+     */
+    public function setTituloAttribute($value)
+    {
+        $this->attributes['firstName'] = ucwords(strtolower($value));
+    }
+
+}
