e9536ef6533c5ee1db0db26c199e15b1c7b1f7ef
iVan Vitali <webcoder.ivan@gmail.com>
2017-04-09 17:29:08 -0300

    dev#8 Se creo el controlador PeliculaController con la dependencia de PeliculaInterface y con el metodo index que muestra la lista de todas las peliculas en la vista /peliculas

---
+++ app/Http/Controllers/PeliculaController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use VideoClub\Repo\PeliculaInterface;
+
+class PeliculaController extends Controller
+{
+    protected $pelicula;
+
+    public function __construct(PeliculaInterface $pelicula)
+    {
+        $this->pelicula = $pelicula;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $peliculas = $this->pelicula->obtenerTodasPeliculas();
+        // Mostrar la lista de todos los miembros
+        return view('pelicula.index')->with('peliculas', $peliculas);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
