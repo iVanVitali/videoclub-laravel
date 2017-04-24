cc9efa56e526281d44e983089bea14e5efc4b7b6
Ivan Vitali <webcoder.ivan@gmail.com>
2017-04-23 21:56:48 -0300

    dev#7 -fix se agregaron los metodos dentro de MovieInterface

--- app/VideoClub/Repo/Pelicula/PeliculaInterface.php
+++ app/VideoClub/Repo/Pelicula/PeliculaInterface.php
@@ -21,4 +21,34 @@
          * @return mixed
          */
         public function agregarPelicula(Request $request);
+
+        /**
+         * @param $id
+         * @return mixed
+         */
+        public function byId($id);
+
+        /**
+         * @param int $page
+         * @param int $limit
+         * @param bool $all
+         * @return mixed
+         */
+        public function byPage($page=1, $limit=10, $all=false);
+
+        /**
+         * @param $slug
+         * @return mixed
+         */
+        public function bySlug($slug);
+
+        /**
+         * @param $tag
+         * @param int $page
+         * @param int $limit
+         * @return mixed
+         */
+        public function byTag($tag, $page=1, $limit=10);
+
+
     }
